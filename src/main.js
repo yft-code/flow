c83443681095e3332f9a770f0d6a8d82f546d4cc
wb.yangfangting01 <wb.yangfangting01@mesg.corp.netease.com>
2026-01-25 13:19:41 +0800

    main

--- src/main.js
+++ src/main.js
@@ -4,11 +4,9 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
-
 Vue.use(ElementUI);
 Vue.config.productionTip = false
-
+console.log('ddddd');
 new Vue({
   router,
   store,
